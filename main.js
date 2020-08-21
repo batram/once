6ea210d5bcebf74e1d2d26cdc99eb00ed2c9ab46
batram <ym@batr.am>
2020-08-21 19:57:21 +0200

    add sliding seperator (pretty clunky) and separate parsers

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow, session } = require("electron")
+const { app, BrowserWindow, session, ipcMain } = require("electron")
 const https = require("https")
 
 require("electron-reload")(__dirname)
@@ -15,33 +15,43 @@ function createWindow() {
     }
   })
 
-  win.webContents.session.webRequest.onHeadersReceived(
-    { urls: ["*://*/*"] },
-    (d, c) => {
-      delete d.responseHeaders["X-Frame-Options"]
-      delete d.responseHeaders["x-frame-options"]
-      delete d.responseHeaders["content-security-policy"]
-      delete d.responseHeaders["content-security-policy"]
-      delete d.responseHeaders["Set-Cookie"]
-      delete d.responseHeaders["Content-Security-Policy"]
-      delete d.responseHeaders["X-Content-Type-Options"]
-      delete d.responseHeaders["X-XSS-Protection"]
-
-      c({
-        cancel: false,
-        responseHeaders: d.responseHeaders
-      })
-    }
-  )
-
   // and load the index.html of the app.
   win.loadFile("index.html")
+  win.webContents.openDevTools()
 
   ElectronBlocker.ElectronBlocker.fromPrebuiltAdsAndTracking(fetch).then(
     (blocker) => {
       blocker.enableBlockingInSession(session.defaultSession)
+
+      win.webContents.session.webRequest.onHeadersReceived(
+        { urls: ["*://*/*"] },
+        (d, c) => {
+          delete d.responseHeaders["X-Frame-Options"]
+          delete d.responseHeaders["x-frame-options"]
+          delete d.responseHeaders["content-security-policy"]
+          delete d.responseHeaders["content-security-policy"]
+          delete d.responseHeaders["Set-Cookie"]
+          delete d.responseHeaders["Content-Security-Policy"]
+          delete d.responseHeaders["X-Content-Type-Options"]
+          delete d.responseHeaders["X-XSS-Protection"]
+
+          c({
+            cancel: false,
+            responseHeaders: d.responseHeaders
+          })
+        }
+      )
     }
   )
+
+  win.webContents.on("found-in-page", (e, result) => {
+    if (result.finalUpdate) {
+      win.webContents.stopFindInPage("keepSelection")
+    }
+  })
+  ipcMain.on("search-text", (e, arg) => {
+    win.webContents.findInPage(arg)
+  })
 }
 
 app.whenReady().then(() => {
---
+++ parser.js
@@ -0,0 +1,55 @@
+module.exports = {
+  parse
+}
+
+let parsers = {
+  "https://news.ycombinator.com/": parse_hn,
+  "https://lobste.rs/": parse_lob
+}
+
+function parse(url, doc) {
+  for (pattern in parsers) {
+    if (url.startsWith(pattern)) {
+      parsers[pattern](doc)
+      break
+    }
+  }
+}
+
+function parse_hn(doc) {
+  let curl = "https://news.ycombinator.com/item?id="
+  let stories = doc.querySelectorAll(".storylink")
+  stories.forEach((x) => {
+    let id = x.parentElement.parentElement.id
+    if (x.protocol == "file:") {
+      x.href = curl + id
+    }
+
+    add_story({
+      type: "HN",
+      href: x.href,
+      title: x.innerText,
+      comment_url: curl + id
+    })
+  })
+}
+
+function parse_lob(doc) {
+  let curl = "https://lobste.rs/s/"
+  let stories = doc.querySelectorAll(".u-url")
+
+  stories.forEach((x) => {
+    let id =
+      x.parentElement.parentElement.parentElement.parentElement.dataset.shortid
+    if (x.protocol == "file:") {
+      x.href = curl + id
+    }
+
+    add_story({
+      type: "LO",
+      href: x.href,
+      title: x.innerText,
+      comment_url: curl + id
+    })
+  })
+}
