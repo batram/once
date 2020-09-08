5fb4dba9561f9e3325bb76360d1d93628e01ca76
batram <ym@batr.am>
2020-09-08 21:58:28 +0200

    use Readability.js + webarchive to outline stories

--- app/js/web_control.js
+++ app/js/web_control.js
@@ -10,6 +10,7 @@ let webview = document.querySelector("#frams")
 webview.addEventListener("did-stop-loading", loadstop)
 
 const outline_api = "https://api.outline.com/v3/parse_article?source_url="
+const data_outline_url = "data:text/html;charset=utf-8,"
 
 webview.addEventListener("console-message", (e) => {
   if (e.message == "mousedown 3") {
@@ -24,7 +25,7 @@ webview.addEventListener("console-message", (e) => {
 })
 
 webview.addEventListener("update-target-url", (event, input) => {
-  //console.log(event, input)
+  console.log(event, input)
 })
 
 webview.addEventListener("new-window", async (e) => {
@@ -33,6 +34,7 @@ webview.addEventListener("new-window", async (e) => {
 
 function loadstop(e) {
   let url = webview.getURL()
+  webview.openDevTools()
   webviewContents = webContents.fromId(webview.getWebContentsId())
 
   webviewContents.on("before-input-event", (event, input) => {
@@ -43,8 +45,56 @@ function loadstop(e) {
     console.log('mousedown', e.button)
   })`)
 
+  let css = `
+  html {
+    margin: 0;
+    padding: 0;
+    display: flex;
+    box-sizing: content-box;
+    width: 100%;    
+  }
+
+  body {
+    margin: 0;
+    padding: 0px 10px;
+    box-sizing: border-box;
+    width: 100%;    
+    display: flex;
+    flex-direction: column;
+  }
+
+  a {
+    color: #6b6bef;
+  }
+
+  img, pre, p {
+    max-width: 100%;
+  }
+
+  pre {
+    overflow-x: auto;
+    padding: 10px;
+  }
+
+  ul {
+    margin: 5px;
+    padding: 0 25px;
+  }
+
+  @media (prefers-color-scheme: dark) {
+    body {
+      color: #bcc2cd;
+      background: #383a59;
+    }
+  }
+`
+
+  webview.insertCSS(css)
+
   if (url.startsWith(outline_api)) {
     webview.executeJavaScript("(" + outline_jshook.toString() + ")()")
+  } else if (url.startsWith(data_outline_url)) {
+    console.log("url outline", url)
   } else {
     urlfield.value = webview.getURL()
   }
@@ -80,7 +130,40 @@ outline_webview_btn.onclick = (x) => {
   }
 }
 
-function outline(url) {
+async function outline(url) {
+  urlfield.value = url
+
+  let f = await fetch("https://archive.org/wayback/available?url=" + url)
+  let resp = await f.json()
+  if (
+    resp.hasOwnProperty("archived_snapshots") &&
+    resp.archived_snapshots.hasOwnProperty("closest") &&
+    resp.archived_snapshots.closest.available
+  ) {
+    let arch_url = new URL(resp.archived_snapshots.closest.url)
+    arch_url.protocol = "https:"
+    console.log("got webarchive", arch_url)
+    url = arch_url
+  }
+
+  let f2 = await fetch(url)
+  let resp2 = await f2.text()
+  let dom_parser = new DOMParser()
+  let doc = dom_parser.parseFromString(resp2, "text/html")
+
+  var article = new Readability(doc).parse()
+  let title = document.createElement("h1")
+  title.innerText = article.title
+  title.classList.add("outlined")
+  console.log(article)
+  webview.loadURL(
+    data_outline_url +
+      encodeURIComponent(title.outerHTML) +
+      encodeURIComponent(article.content)
+  )
+}
+
+function outline_fallback(url) {
   urlfield.value = url
   let options = {
     httpReferrer: "https://outline.com/",
@@ -92,6 +175,8 @@ function outline(url) {
 
 function outline_jshook() {
   let data = JSON.parse(document.body.innerText).data
-  document.body.innerHTML = "<h1>" + data.title + "</h1>"
+  let title = document.createElement("h1")
+  title.innerText = data.title
+  document.body.innerHTML = title.outerHTML
   document.body.innerHTML += data.html
 }
