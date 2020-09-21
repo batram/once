ded99869c4feb7acfe2814c86ea770decea99c30
batram <ym@batr.am>
2020-09-21 09:30:03 +0200

    animate click on menu for panel

--- src/js/view/menu.ts
+++ src/js/view/menu.ts
@@ -1,3 +1,4 @@
+import { target } from "on-change"
 import * as search from "../data/search"
 import * as seperation_slider from "../view/sep_slider"
 
@@ -5,13 +6,17 @@ export { add_tag, open_panel, init }
 
 function open_panel(panel: string) {
   seperation_slider.expand_left()
-  document.querySelectorAll<HTMLElement>("#left_main .panel").forEach((x) => {
-    if (x.dataset.panel != panel) {
-      x.style.display = "none"
-    } else {
-      x.style.display = "flex"
-    }
-  })
+  let left_panel = document.querySelector<HTMLElement>("#left_panel")
+  left_panel.setAttribute("active_panel", panel)
+}
+
+function highlight_panel(panel: string) {
+  let target_panel = document.querySelector<HTMLElement>("#" + panel + "_panel")
+  target_panel.classList.add("pseudo_active")
+}
+function delight_panel(panel: string) {
+  let target_panel = document.querySelector<HTMLElement>("#" + panel + "_panel")
+  target_panel.classList.remove("pseudo_active")
 }
 
 function add_tag(type: string, colors: [string, string]) {
@@ -25,6 +30,7 @@ function add_tag(type: string, colors: [string, string]) {
     tag.innerText = "[" + type + "]"
     tag.style.backgroundColor = colors[0]
     tag.style.color = colors[1]
+    tag.dataset.panel = "stories"
 
     if (colors && colors[0] != "") {
       //inject css for story tags
@@ -52,20 +58,35 @@ function add_tag(type: string, colors: [string, string]) {
       searchfield.value = "[" + type + "]"
       search.search_stories("[" + type + "]")
     }
-
+    active_flash_panel(tag)
     document.querySelector("#menu").appendChild(tag)
   }
 }
 
+function active_flash_panel(btn: HTMLElement) {
+  btn.onmousedown = (e) => {
+    highlight_panel(btn.dataset.panel)
+  }
+  btn.onmouseup = (e) => {
+    delight_panel(btn.dataset.panel)
+  }
+  btn.onmouseout = (e) => {
+    delight_panel(btn.dataset.panel)
+  }
+}
+
 function init() {
-  document.querySelectorAll<HTMLElement>("#menu .sub").forEach((x) => {
-    x.onclick = (e) => {
-      open_panel(x.dataset.panel)
+  document.querySelectorAll<HTMLElement>("#menu .sub").forEach((sub_menu) => {
+    sub_menu.onclick = (e) => {
+      open_panel(sub_menu.dataset.panel)
     }
-    x.querySelectorAll("img").forEach((x) => {
+    sub_menu.querySelectorAll("img").forEach((x) => {
       x.setAttribute("draggable", "false")
     })
+    active_flash_panel(sub_menu)
   })
+
+  open_panel("stories")
 
   if (document.querySelector("#menu")) {
     //Add special tags for search
