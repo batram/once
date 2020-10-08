a599ef99c12490787a180bb1939f5fbc1a22ed7b
batram <ym@batr.am>
2020-10-08 12:17:50 +0200

    No more hide and seek in contextmenu
    Just build the menu each time with the Items that are needed for the context

--- src/js/view/contextmenu.ts
+++ src/js/view/contextmenu.ts
@@ -42,7 +42,6 @@ const cmenu_data: CMenuData = {
   wc_id: null,
 }
 
-//Dafug why event: Electron.KeyboardEvent, ELectron why???
 function inspect(_menuItem: Electron.MenuItem, cwin: Electron.BrowserWindow) {
   let wc = cwin.webContents
 
@@ -59,90 +58,66 @@ function inspect(_menuItem: Electron.MenuItem, cwin: Electron.BrowserWindow) {
   }
 }
 
-const con_menu = new Menu()
-
-con_menu.append(
-  new MenuItem({
-    label: "inspect",
-    click: inspect,
-  })
-)
-
-con_menu.append(
-  new MenuItem({ type: "separator", id: "url_sep", visible: false })
-)
-con_menu.append(
-  new MenuItem({
-    id: "url_copy",
-    label: "Copy URL",
-    visible: false,
-    click() {
-      clipboard.writeText(cmenu_data.href, "selection")
-    },
-  })
-)
-con_menu.append(
-  new MenuItem({
-    id: "url_open",
-    label: "Open in Browser",
-    visible: false,
-    click: async () => {
-      await shell.openExternal(cmenu_data.href)
-    },
-  })
-)
-
-con_menu.append(
-  new MenuItem({ type: "separator", id: "tab_sep", visible: false })
-)
-con_menu.append(
-  new MenuItem({
-    id: "tab_close",
-    label: "Close Tab",
-    visible: false,
-    click() {
-      cmenu_data.sender.send("tab_close", cmenu_data.wc_id)
-    },
-  })
-)
-con_menu.append(
-  new MenuItem({
-    id: "tab_dupe",
-    label: "Duplicate Tab",
-    visible: false,
-    click: async () => {
-      cmenu_data.sender.send("tab_dupe", cmenu_data.wc_id)
-    },
-  })
-)
-
 function inspect_menu(
   sender: Electron.WebContents,
   params: { linkURL?: string; x?: number; y?: number; wc_id?: string }
 ) {
   cmenu_data.sender = sender
+  const con_menu = new Menu()
+  con_menu.append(
+    new MenuItem({
+      label: "inspect",
+      click: inspect,
+    })
+  )
 
   if (params.linkURL && params.linkURL != "") {
-    con_menu.getMenuItemById("url_sep").visible = false
-    con_menu.getMenuItemById("url_copy").visible = true
-    con_menu.getMenuItemById("url_open").visible = true
+    con_menu.append(new MenuItem({ type: "separator", id: "url_sep" }))
+    con_menu.append(
+      new MenuItem({
+        id: "url_copy",
+        label: "Copy URL",
+        click() {
+          clipboard.writeText(cmenu_data.href, "selection")
+        },
+      })
+    )
+    con_menu.append(
+      new MenuItem({
+        id: "url_open",
+        label: "Open in Browser",
+        click: async () => {
+          await shell.openExternal(cmenu_data.href)
+        },
+      })
+    )
     cmenu_data.href = params.linkURL
   } else {
-    con_menu.getMenuItemById("url_sep").visible = false
-    con_menu.getMenuItemById("url_copy").visible = false
-    con_menu.getMenuItemById("url_open").visible = false
     cmenu_data.href = null
   }
 
   if (params.wc_id && params.wc_id != "") {
-    con_menu.getMenuItemById("tab_sep").visible = true
-    con_menu.getMenuItemById("tab_close").visible = true
-    con_menu.getMenuItemById("tab_dupe").visible = true
+    con_menu.append(new MenuItem({ type: "separator", id: "tab_sep" }))
+    con_menu.append(
+      new MenuItem({
+        id: "tab_close",
+        label: "Close Tab",
+        click() {
+          cmenu_data.sender.send("tab_close", cmenu_data.wc_id)
+        },
+      })
+    )
+    con_menu.append(
+      new MenuItem({
+        id: "tab_dupe",
+        label: "Duplicate Tab",
+        click: async () => {
+          cmenu_data.sender.send("tab_dupe", cmenu_data.wc_id)
+        },
+      })
+    )
     cmenu_data.wc_id = params.wc_id
   } else {
-    con_menu.getMenuItemById("tab_sep").visible = false
-    con_menu.getMenuItemById("tab_close").visible = false
-    con_menu.getMenuItemById("tab_dupe").visible = false
     cmenu_data.wc_id = null
   }
 
@@ -150,5 +125,6 @@ function inspect_menu(
     x: params.x,
     y: params.y,
   }
+
   con_menu.popup()
 }
