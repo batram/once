ea987eb766e8c68b4a951d83df07f665f254dd42
batram <1382274+batram@users.noreply.github.com>
2026-07-29 22:50:47 +0200

    Tighten package export surfaces

--- packages/platform-webext/src/index.ts
+++ packages/platform-webext/src/index.ts
@@ -1,4 +1,3 @@
-export { IndexedDbCacheStore as CacheStore } from "@once/persistence"
 export * from "./storage/WebExtSyncStorage"
 export * from "./ui/WebExtTheme"
 export * from "./WebExtPlatform"
--- packages/ui-web/src/index.ts
+++ packages/ui-web/src/index.ts
@@ -1,55 +1,23 @@
 export { StoryListItem } from "./StoryListItem"
 export * as StoryList from "./StoryList"
-export { StoryHistory } from "./StoryHistory"
 export * from "./StoryContextMenu"
 export {
   openStoryAnchoredMenu,
-  openAnchoredMenu,
   closeStoryAnchoredMenu,
-  isStoryAnchoredMenuOpen,
-  AnchoredMenuItem,
-  AnchoredMenuOptions,
-  StoryAnchoredMenuOptions
+  isStoryAnchoredMenuOpen
 } from "./StoryAnchoredMenu"
-export { SettingsPanel } from "./SettingsPanel"
-export {
-  parseSourceGroups,
-  serializeSourceGroups,
-  parseFilterRows,
-  parseRedirectRows,
-  serializeRedirectRows
-} from "./StructuredSettingsEditors"
-export { LoaderInsights } from "./LoaderInsights"
 export { HoverUrlIndicator } from "./HoverUrlIndicator"
-export { addCollectorColorStyles } from "./CollectorStyles"
-export {
-  showConfirmDialog,
-  showTextInputDialog,
-  ConfirmDialogOptions,
-  TextInputDialogOptions
-} from "./ConfirmDialog"
 export * as Search from "./search"
 export * as Menu from "./menu"
-export { setOnceClient, getOnceClient } from "./client"
-export { mountOnceUi, MountOnceUiOptions } from "./mountOnceUi"
+export { getOnceClient } from "./client"
+export { mountOnceUi } from "./mountOnceUi"
 export { bindMenuCollapseControls } from "./MenuCollapse"
-export {
-  AppUpdater,
-  AppUpdateState,
-  AppUpdateStatus,
-  bindAppUpdateControls
-} from "./AppUpdateControls"
 export { ReaderView } from "./reader/ReaderView"
 export { ReaderDocumentHost } from "./ReaderDocumentHost"
 export {
   ReadingSession,
   ReadingSessionState,
-  ReadingMode,
-  ReadingLoadState,
   ReadingRequestEvent,
-  READING_REQUEST,
-  requestReading
+  READING_REQUEST
 } from "./ReadingSession"
 export { SourcePickerView } from "./picker/SourcePickerView"
-export { extractArticle, ReaderArticle } from "./reader/extractArticle"
-export { readerDocument, ReaderTheme } from "./reader/readerDocument"
